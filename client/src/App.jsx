5b7dc1142286b77800aebbc84401f508c206e372
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-20 16:33:51 +0530

    Add frontend files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import JobList from "./pages/JobList";
+import JobDetail from "./pages/JobDetail";
+import Dashboard from "./pages/Dashboard";
+import PostJob from "./pages/Applications";
+import Application from "./pages/Applications";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return (
-    <h1>Full Stack Job Portal</h1>
+   return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/jobs" element={<JobList />} />
+        <Route path="/jobs/:id" element={<JobDetail />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/post-job" element={<PostJob />} />
+        <Route
+          path="/applications"
+          element={<Applications />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
