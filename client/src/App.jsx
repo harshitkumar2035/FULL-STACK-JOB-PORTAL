8196eb5a703b89699af0a794f30d2b36927e998e
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-20 14:56:06 +0530

    Setup React frontend with Vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-// Pages
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import JobList from "./pages/JobList";
-import JobDetail from "./pages/JobDetail";
-import PostJob from "./pages/PostJob";
-import Applications from "./pages/Applications";
-
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Navbar /> {/* Navigation bar */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/jobs" element={<JobList />} />
-          <Route path="/jobs/:id" element={<JobDetail />} />
-
-          {/* Protected Routes */}
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/post-job"
-            element={
-              <ProtectedRoute allowedRoles={["recruiter"]}>
-                <PostJob />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/applications"
-            element={
-              <ProtectedRoute>
-                <Applications />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <h1>Full Stack Job Portal</h1>
   );
 }
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRouters.js");
 
 
 app.use("/api/auth", authRoutes);
