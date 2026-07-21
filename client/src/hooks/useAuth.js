c4a20216214ae144b1f908ef8a851f423e3ffc71
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-21 22:09:00 +0530

    Add frontend files

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,9 @@
+function useAuth() {
+    return (
+        <nav>
+            <h2>use Auth</h2>
+        </nav>
+    );
+}
+
+export default useAuth;
---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,9 @@
+function useFetch() {
+    return (
+        <nav>
+            <h2>use Fetch</h2>
+        </nav>
+    );
+}
+
+export default useFetch;
