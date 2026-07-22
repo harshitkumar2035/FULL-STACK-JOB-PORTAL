89eb2ec1902a9bd614a95662cc4490db625e6b82
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-22 22:50:58 +0530

    Add frontend files

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,43 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
 function Navbar() {
     return (
-        <nav>
-            <h2>Job Portal</h2>
-        </nav>
+          <nav className="navbar">
+
+      <div className="logo">
+        JobPortal
+      </div>
+
+      <ul className="nav-links">
+
+        <li>
+          <NavLink to="/">
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/jobs">
+            Jobs
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/login">
+            Login
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/register">
+            Register
+          </NavLink>
+        </li>
+
+      </ul>
+
+    </nav>
     );
 }
 
