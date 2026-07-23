5c87bc0244f3c30b03b91833a9691e0a4f66e890
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-23 11:17:50 +0530

    Add frontend files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import "./index.css";
+import App from "./App.jsx";
+
+import AuthProvider from "./context/AuthContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
 );
-
