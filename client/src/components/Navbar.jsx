acacfdfedf33fe985d971d6501817dd04d2f813d
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-23 15:51:12 +0530

    Add frontend fils

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,26 +4,43 @@ import "./Navbar.css";
 
 function Navbar() {
   const { user } = useAuth();
-  console.log(user);
-  return (
+    
+     return (
     <nav className="navbar">
-      <div className="logo">
-        JobPortal
+      <div className="container navbar-container">
+        <div className="logo">
+          JobPortal
+        </div>
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/jobs">Jobs</NavLink>
+          </li>
+          {!user ? (
+            <>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Register</NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard">Dashboard</NavLink>
+              </li>
+              <li>
+                <button>
+                  Logout
+                </button>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
-      <ul className="nav-links">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/jobs">Jobs</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Register</NavLink>
-        </li>
-      </ul>
     </nav>
   );
 }
