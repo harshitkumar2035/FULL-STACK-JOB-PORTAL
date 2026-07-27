9746b27436875c715ae203e6593f56df93c89d5c
Harshit Kumar <harshitsv2005@gmail.com>
2026-07-27 17:20:24 +0530

    Add full stack job portal files

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,5 +1,5 @@
 const Job = require("../models/Job");
-const Application = require("../models/application");
+const Application = require("../models/Application");
 
 const getRecruiterStats = async (req, res) => {
   try {
